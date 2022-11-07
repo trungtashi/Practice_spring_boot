a3582e7646434f1c9ac856e1dec8e8f92c851674
trungtashi99 <trungtashi@gmai.com>
2022-11-07 08:52:18 +0700

    Practice spring boot exception

---
+++ src/main/java/com/example/practice_spring_boot_exception/PracticeSpringBootExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_spring_boot_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringBootExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_spring_boot_exception/PracticeSpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_spring_boot_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
